9d3509342b47cdbf54316194bec3886d4bca34dc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-05-26 16:15:47 +0300

    add a blinking cursor after the last digit of the input number

--- src/js/CalculationView.js
+++ src/js/CalculationView.js
@@ -4,7 +4,15 @@ export default class CalculationView {
         this.expressionEl = calcEl.querySelector('.calc-curr-expression');
         this.missingParensEl = calcEl.querySelector('.calc-missing-parens')
         this.leftParenEl = calcEl.querySelector('.calc-btn-left-paren');
+        this.cursor = this.createCursor();
         this.update(modelData);
+    }
+
+    createCursor() {
+        const span = document.createElement('span');
+        span.innerText = '_';
+        span.className = "cursor";
+        return span;
     }
 
     getOperand() {
@@ -19,12 +27,13 @@ export default class CalculationView {
         let s = data.operand.toString();
         if (this.getOperand() !== s) {
             this.operandEl.textContent = s;
+            this.operandEl.appendChild(this.cursor);
         }
         s = data.expression.toString();
         if (this.getExpression() !== s) {
             this.expressionEl.textContent = s;
         }
-        if (data.leftParenCount !== 0) {
+        if (data.leftParenCount !== 0 ) {
             let parentheses = '';
             for (let i = 0; i < data.leftParenCount; i++) {
                 parentheses += ')';
@@ -34,9 +43,7 @@ export default class CalculationView {
             this.leftParenEl.style.setProperty("--content", content);
         } else {
             this.leftParenEl.style.setProperty("--content", "''");
-            if (this.missingParensEl.textContent !== '') {
-                this.missingParensEl.textContent = '';
-            }
+            this.missingParensEl.textContent = '';
         }
     }
 }
