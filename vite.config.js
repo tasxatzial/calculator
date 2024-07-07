bb594b367ccbeb3899458b3954e3fd45eebde571
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-07-07 19:13:37 +0300

    remove rollup-plugin-licence, update package*.json files

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite';
-import license from 'rollup-plugin-license';
-import path from 'path';
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      plugins: [
-          license({
-          thirdParty: {
-              includePrivate: true,
-              output: {
-                file: path.join(__dirname, 'dist', 'dependencies.txt'),
-              },
-            },
-          }),
-        ],
-    },
+    assetsInlineLimit: 0,
   },
 });
