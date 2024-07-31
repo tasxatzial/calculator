0017ace1744d08aa020fad8ec023107981cfa803
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-07-31 15:17:26 +0300

    add vitest and a github workflow

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: "src",
+  base: "/calculator/",
   build: {
     emptyOutDir: true,
     outDir: '../dist',
     assetsInlineLimit: 0,
   },
+  test: {
+    include: ['../tests/**/*.test.js'],
+    coverage: {
+      enabled: true,
+      reportsDirectory: "../coverage",
+      reporter: ['html']
+    },
+  }
 });
